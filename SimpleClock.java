7386387cb5915160d5ee667cf4b092e8b8b6a174
Angelinamathew <angelinamathew707@gmail.com>
2024-04-01 23:09:58 -0400

    Updated

--- SimpleClock.java
+++ SimpleClock.java
@@ -1,9 +1,12 @@
 //package SimpleClock;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 
 public class SimpleClock extends JFrame {
@@ -16,35 +19,48 @@ public class SimpleClock extends JFrame {
         JLabel timeLabel;
         JLabel dayLabel;
         JLabel dateLabel;
+
         String time;
         String day;
         String date;
+
+        JButton button1;
+        JButton button2;
+
 
         SimpleClock() {
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setTitle("Digital Clock");
             this.setLayout(new FlowLayout());
-            this.setSize(350, 220);
+            this.setSize(500, 350);
             this.setResizable(false);
-    
+
+
             timeFormat = new SimpleDateFormat("hh:mm:ss a");
+
             dayFormat=new SimpleDateFormat("EEEE");
             dateFormat=new SimpleDateFormat("dd MMMMM, yyyy");
             timeLabel = new JLabel();
-            timeLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 59));
+            timeLabel.setFont(new Font("TIMES ROMAN", Font.BOLD, 59));
             timeLabel.setBackground(Color.BLACK);
-            timeLabel.setForeground(Color.WHITE);
+            timeLabel.setForeground(Color.GREEN);
             timeLabel.setOpaque(true);
             dayLabel=new JLabel();
             dayLabel.setFont(new Font("Ink Free",Font.BOLD,34));
     
             dateLabel=new JLabel();
             dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
+
+
+            JButton button = new JButton("12/24");
+
+
     
     
             this.add(timeLabel);
             this.add(dayLabel);
             this.add(dateLabel);
+            this.add(button);
             this.setVisible(true);
     
             setTimer();
